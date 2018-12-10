7deb0e02979195feb691ee2b69b7cb4047a5001a
Siarhei.Klybik <Siarhei.Klybik@itechart-group.com>
2018-12-10 12:10:09 +0300

    take Screenshot after fail

--- conf.js
+++ conf.js
@@ -1,6 +1,18 @@
 let config = require('./config');
 let AllureReporter = require('jasmine-allure-reporter');
 
+let addScreenShots = new function () {
+    this.specDone = function (result) {
+        if (result.status === "failed") {
+            browser.takeScreenshot().then(function (png) {
+                allure.createAttachment('Screenshot', function () {
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+            });
+        }
+    };
+};
+
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -21,21 +33,14 @@ exports.config = {
     onPrepare: () => {
         // jasmine.getEnv().addReporter(DescribeFailureReporter(jasmine.getEnv()));
         jasmine.getEnv().addReporter(new AllureReporter());
-        jasmine.getEnv().afterAll(function(done) {
-            console.log(jasmine.getEnv().currentSpec.results());
-            // if (!jasmine.getEnv().currentSpec.results().passed()) {
-
-                browser.takeScreenshot().then(function (png) {
-                    allure.createAttachment('Screenshot', function () {
-                        return new Buffer(png, 'base64')
-                    }, 'image/png')();
-                    done();
-                })
-            // }
-        });
+        jasmine.getEnv().addReporter(addScreenShots);
+        // jasmine.getEnv().afterAll(function(done) {
+                // browser.takeScreenshot().then(function (png) {
+                //     allure.createAttachment('Screenshot', function () {
+                //         return new Buffer(png, 'base64')
+                //     }, 'image/png')();
+                //     done();
+                // })
+        // });
     }
-    // browser.driver.manage().timeouts().implicitlyWait(20000);
-    // browser.waitForAngularEnabled(false);
-        // browser.manage().window().maximize();
-    // }
 };
