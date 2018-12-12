8ea9a6b532b12fdb56ed10d5270ee6982ae537ce
Siarhei.Klybik <Siarhei.Klybik@itechart-group.com>
2018-12-12 15:46:00 +0300

    Change ComposeForm locator to css

--- project/forms/composeForm.js
+++ project/forms/composeForm.js
@@ -5,14 +5,14 @@ const pageHelper = require('../../framework/helpers/pageHelper');
 
 class ComposeForm extends BasePage{
     constructor() {
-        super(element(by.id("b-compose")), "Compose letter form");
-        this.txbTo = element(by.xpath("//textarea[@data-original-name='To']"));
-        this.lblTo = element(by.id("compose_to"));
-        this.txbSubject = element(by.xpath("//input[@name='Subject']"));
-        this.txbMessage = element(by.id("tinymce"));
-        this.btnSaveDraft = element(by.xpath("//div[contains(@id,'toolbar')]//div[@data-name='saveDraft']"));
-        this.lblSaveStatus = element(by.xpath("//div[@data-mnemo='saveStatus']//span[@class='time']"));
-        this.iFrameComposeEditor = element(by.xpath("//iframe[contains(@id, 'composeEditor_ifr')]"));
+        super($("#b-compose"), "Compose letter form");
+        this.txbTo = $("textarea[data-original-name='To']");
+        this.lblTo = $("#compose_to");
+        this.txbSubject = $("input[name='Subject']");
+        this.txbMessage = $("#tinymce");
+        this.btnSaveDraft = $("[id*='toolbar'] [data-name='saveDraft']");
+        this.lblSaveStatus = $$("[data-mnemo='saveStatus'] .time").first();
+        this.iFrameComposeEditor = $("iframe[id*='composeEditor_ifr']");
     }
 
     typeInTo(email) {
